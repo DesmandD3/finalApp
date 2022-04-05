576e5474f4c2809b02d99a5aaf13902ae7d8708c
Carlosfromsubway <tlee285@my.bcit.ca>
2022-04-05 12:38:07 -0700

    startbutton

---
+++ comps/start-button.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+export default function StartButton() {
+
+const StyledStart = styled.button`
+width: 200px;
+height: 80px;
+background-color: #71F099;
+color: #F5FCC9;
+`;
+
+return (
+    <button> 
+        Start
+    </button>
+    
+);
+}
---
+++ pages/start.js
@@ -0,0 +1,9 @@
+import StartButton from '../comps/start-button';
+export default function Start(){ 
+
+
+<div> 
+    <h1> hi</h1>
+<StartButton />
+</div>
+}
