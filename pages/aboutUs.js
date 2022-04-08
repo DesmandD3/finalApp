eff3938e4f0cb0b70eff0e6855f1a648781b2ebc
Desmand <dking68@my.bcit.ca>
2022-04-07 17:33:19 -0700

    About us page(Started)

---
+++ pages/aboutUs.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import Back from "../comps/Back";
+const FullPage = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Fira+Sans:wght@300;400&display=swap");
+  background-color: #222e50;
+  text-align: center;
+  font-family: "Fira Sans", sans-serif;
+`;
+
+const Header = styled.h1`
+  font-size: 48px;
+  color: fefae0;
+  text-align: center;
+  color: #fefae0;
+`;
+
+const PageTitle = styled.h2`
+  font-size: 24px;
+  color: #fefae0;
+`;
+
+const Names = styled.p`
+  font-size: 18px;
+  color: #fefae0;
+`;
+
+export default function About() {
+  return (
+    <FullPage>
+      <PageTitle>About Us</PageTitle>
+      <Header> Pathfinders </Header>
+      <PageTitle> Imagine the Story </PageTitle>
+      <Names>
+        Aly Ali <br></br>
+        Desmand King <br></br>
+        Tyler Lee <br></br>
+        Justin Ma <br></br>
+        John Sustituido <br></br>
+      </Names>
+      <Names>
+        MDIA - 2106<br></br>
+        Set - A
+      </Names>
+      <Back></Back>
+    </FullPage>
+  );
+}
