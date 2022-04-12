f3a242c0b458afa6fdb774cb16bf65d772a35623
Carlosfromsubway <tlee285@my.bcit.ca>
2022-04-12 12:37:35 -0700

    textbub

--- comps/Textbubble.js
+++ comps/Textbubble.js
@@ -9,17 +9,9 @@ font-size: 22px;
 margin: 10px;
 padding: 10px;
 border-radius: 10px;
+position: relative;
 
-div:after{
-    content: " ";
-    position:absolute;
-    right:30px;
-    bottom -15;
-    border-top:15px solid transparent;
-    border-right:15px solid transparent;
-    border-left:15px solid transparent;
-    bprder-radius:1px;
-}
+
 `;
 
 const triangle = styled.div`
@@ -34,4 +26,5 @@ export default function (){
         hbwariubgoweabngvioewarngipeanrg'hgerbgwaoerng'
         jnetsjnesrnrsthnrsthnrtshtes
     </Textbub> 
+   
 }
