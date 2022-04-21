69c116a6882033b254d16894afe5c92d7fa4ce78
Carlosfromsubway <tlee285@my.bcit.ca>
2022-04-21 16:45:55 -0700

    ways to help button

--- comps/waysButton.js
+++ comps/waysButton.js
@@ -1,14 +1,18 @@
 import styled from "styled-components";
 
-const WButton = styled.button`
+const WayButton = styled.button`
   background-color: #41658a;
   color: #fefae0;
   border-radius: 10px;
-  width: 200px;
-  height: 70px;
+  width: 180px;
+  height: 60px;
+  font-size: 22px;
   border-color: #71f099;
+  margin: 5px;
+  border-width: 2px;
 `;
 
-export default function WayButton() {
-  <WButton>Ways To Help</WButton>;
+export default function WButton() {
+  return  <WayButton>Ways To Help</WayButton>;
+  
 }
