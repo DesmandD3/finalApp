c556910e3b7aa6293fc6b3c3f8819a5d858ffbee
Desmand <dking68@my.bcit.ca>
2022-04-21 17:38:36 -0700

    evening commit

--- pages/test.js
+++ pages/test.js
@@ -3,11 +3,7 @@ import SmallNext from "../comps/smNext";
 import Textbub from "../comps/Textbubble";
 import BackButton from "../comps/Back";
 import StartButton from "../comps/start-button";
-<<<<<<< HEAD
-import WButton from "../comps/waysButton";
-=======
 import WayButton from "../comps/waysButton";
->>>>>>> 4a2eb422a1b16fa008a107f00c0db39251e0fc2b
 
 export default function test() {
   return (
@@ -20,11 +16,6 @@ export default function test() {
       <WayButton />
       <BackButton />
       <StartButton />
-<<<<<<< HEAD
-      <WButton />
-=======
-      
->>>>>>> 4a2eb422a1b16fa008a107f00c0db39251e0fc2b
     </div>
   );
 }
