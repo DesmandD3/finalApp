61d03e9f1b9bb3bab9ba140b8e65411885032630
JohnRayissus <jsustituido@my.bcit.ca>
2022-04-22 20:52:35 -0700

    navbar

---
+++ comps/nav/navbar.js
@@ -0,0 +1,30 @@
+import Next from 'next'
+import React from 'react'
+import styled from 'styled-components'
+import Burger from './burger'
+
+const Nav = styled.nav`
+width:100%;
+height:65px;
+border-bottom:2px solid #f1f1f1;
+padding:0 20px;
+display:flex;
+justify-content:space-between;
+
+.hamLogo {
+    padding:15px 0;
+}
+`
+
+const Navbar = () => {
+    return (
+        <Nav>
+           <div className="hamLogo">
+               Nav Bar
+           </div>
+           <Burger />
+           </Nav>
+    )
+}
+
+export default Navbar;
---
+++ pages/testpage.js
@@ -0,0 +1,14 @@
+import Next from 'next'
+import Navbar from '../comps/Nav/navbar'
+import React from 'react'
+
+function App() {
+    return (
+        <div className="App">
+            <Navbar />
+            App Page
+        </div>
+    )
+}
+
+export default App;
