2f8bc7eeeedbfe41a99244f4ad289f2cdb537a3d
Desmand <dking68@my.bcit.ca>
2022-04-24 15:50:54 -0700

    changes start to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,55 @@
 import StartButton from "../comps/start-button";
-import styles from "../styles/Home.module.css";
 import Leafs from "../comps/leafs";
+import styled from "styled-components";
+import Moon from "../comps/Moon";
+import Lamp from "../comps/Lamp";
+import Navbar from "../comps/nav/navbar";
 
-export default function Home() {
+const FullPage = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Fira+Sans:wght@300;400&display=swap");
+  background: linear-gradient(
+    to bottom,
+    #003472 0%,
+    #003472 70%,
+    #758998 70%,
+    #758998 100%
+  );
+  text-align: center;
+  font-family: "Fira Sans", sans-serif;
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  align-items: center;
+  max-width: auto;
+  height: 844px;
+  overflow: hidden;
+`;
+
+const Header = styled.h1`
+  font-size: 48px;
+  margin-top: 180px;
+  color: fefae0;
+  color: #fefae0;
+`;
+
+const PageTitle = styled.h2`
+  font-size: 24px;
+  color: #fefae0;
+  margin-bottom: 120px;
+`;
+
+export default function Start() {
   return (
-    <div className={styles.container}>
+    <div>
       <Leafs />
-
-      <StartButton />
-
-      <main className={styles.main}></main>
+      <FullPage>
+        <Navbar />
+        <Lamp />
+        <Moon />
+        <Header>Pathfinders</Header>
+        <PageTitle>Imagine the Story</PageTitle>
+        <StartButton></StartButton>
+      </FullPage>
     </div>
   );
 }
