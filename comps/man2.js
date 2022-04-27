33ea93be6d8bd80a4a071e6d4df6403cc90d3ec3
Sheraliramzan <72282103+Sheraliramzan@users.noreply.github.com>
2022-04-26 17:22:19 -0700

    Update

---
+++ comps/man2.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+function Man2() {
+  return (
+    <div>
+      <img
+        style={{
+          display: "flex",
+          position: "absolute",
+          top: 175,
+          right: 180,
+          width: 300,
+          zIndex: 1,
+          objectFit: "contain",
+        }}
+        src="/Man.svg"
+      />
+    </div>
+  );
+}
+export default Man2;
---
+++ comps/textresult1.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+function TextResult() {
+  return (
+    <div>
+      <img
+        style={{
+          overflow: "hidden",
+          display:"flex",
+          position:"relative",
+          
+
+        }}
+        src="/asset10.png"
+      />
+    </div>
+  );
+}
+export default TextResult;
