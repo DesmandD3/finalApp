36fb8892c4af9c4a95a77fd10862c9d6cbcc2b6d
JohnRayissus <jsustituido@my.bcit.ca>
2022-04-26 19:58:17 -0700

    green back button

--- comps/Back.js
+++ comps/Back.js
@@ -1,15 +1,24 @@
 import styled from "styled-components";
 import { useRouter } from "next/router";
-const Back = styled.button`
+
+const Start = styled.button`
   width: 200px;
   height: 70px;
   background-color: #71f099;
   color: #fefae0;
   font-size: 40px;
   border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 600px;
 `;
 
-export default function BackButton() {
+export default function StartButton({ path = "" }) {
   const r = useRouter();
-  return <Back onClick={() => r.replace({ pathname: "/start" })}>Back</Back>;
+  return (
+    <Start onClick={() => r.replace({ pathname: path })}> Back </Start>
+  );
 }
+
